37f8a84451a5db8bbe19894a36452a3bff58c5db
OneTwoZzzPlus <ivansakulin5505@gmail.com>
2026-01-27 12:03:18 +0300

    Added err connection tip

--- src/strings.js
+++ src/strings.js
@@ -1,6 +1,7 @@
 export const loadingText = "Загружаем...";
 
 const tip = "<br/>(обновите расширение/сайт)";
+const tipConnection = "<br/>Попробуйте ещё раз (проверьте подключение к сети Интернет, VPN, анти-DPI, ограничения провайдера)";
 
 export const fewCharactersText = "Введите хотя бы 3 символа =]";
 export const unknownTypeText = `<span class="error">Не понятно, что это такое :|</span>` + tip;
@@ -13,7 +14,7 @@ export const emptyCommentsList = "Отзывы отсутствуют \\(O_o)/";
 /** Ошибки при загрузке отзывов **/
 export function statusReviewsText(status) {
     switch (status) {
-        case 0: return `<span class="error">Сервер с отзывами недоступен =(</span>`;
+        case 0: return `<span class="error">Сервер с отзывами недоступен =(</span>` + tipConnection;
         case 401: return "Сначала войдите, это быстро =)";
         case 404: return "Отзывы отсутствуют \\(O_o)/";
         default: return `Сервер прислал "${status}" вместо отзывов =(`;
@@ -23,7 +24,7 @@ export function statusReviewsText(status) {
 /** Ошибки при загрузке поиска **/
 export function statusSearchText(status) {
     switch (status) {
-        case 0: return `<span class="error">Сервер с отзывами недоступен =(</span>`;
+        case 0: return `<span class="error">Сервер с отзывами недоступен =(</span>` + tipConnection;
         case 401: return "Сначала войдите, это быстро =)";
         case 404: return "Ничего не найдено \\(O_o)/";
         default: return `Сервер прислал "${status}" вместо результатов поиска =(`;
